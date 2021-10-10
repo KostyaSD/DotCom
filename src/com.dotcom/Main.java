c01d62084ba7629b8ab1d728f6efcd6ba8523867
RazerPro <Drakula3D@mail.ru>
2021-10-10 14:00:27 +0300

    version 0.1

--- src/com.dotcom/Main.java
+++ src/com.dotcom/Main.java
@@ -2,6 +2,6 @@ package com.dotcom;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello System");
+        System.out.println("Hello System one");
     }
 }
